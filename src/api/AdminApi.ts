c3026cde6c7805a412c7332f9c477f80a2b6ba62
Sergiu <sergiu.ojoc@gmail.com>
2025-03-22 05:13:33 +0200

    Starting with upload photos. Need to get back to the authentication process

--- src/api/AdminApi.ts
+++ src/api/AdminApi.ts
@@ -1,5 +1,22 @@
+import { handleResponse } from "./Api.ts";
+
+const baseUrl = "http://localhost:8000/api/admin";
+
 const adminApi = {
-  get: (url: string) => {}
+  get: (url: string) => {},
+  post: async (url: string) => {
+    const apiUrl = `${baseUrl}/${url}`;
+    const response = await fetch(apiUrl, {
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      }
+    });
+
+    return handleResponse(response);
+  }
 };
 
 export default adminApi;
--- src/routes/PhotoUpload/PhotoUpload.tsx
+++ src/routes/PhotoUpload/PhotoUpload.tsx
@@ -1,5 +1,35 @@
+import { useDropzone } from "react-dropzone";
+
 const PhotoUpload = () => {
-  return <p>Here is photo upload</p>;
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map((file) => {
+    return (
+      <li key={file.path}>
+        <>
+          <img src={URL.createObjectURL(file)} alt="" width="100" />
+          {file.path}
+        </>
+      </li>
+    );
+  });
+
+  return (
+    <div className="drop-wrapper">
+      <section className="container">
+        <div {...getRootProps({ className: "dropzone" })}>
+          <input {...getInputProps()} />
+          <p>Drag and drop some files here, or click to select files</p>
+        </div>
+      </section>
+      <div className="photos-list">
+        <aside>
+          <h4>Files</h4>
+          <ul>{files}</ul>
+        </aside>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoUpload;
