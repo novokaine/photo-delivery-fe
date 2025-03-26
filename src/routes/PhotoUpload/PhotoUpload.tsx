cfc801f2165eee7fba31cfd1d40522c24d099f79
Sergiu <sergiu.ojoc@gmail.com>
2025-03-26 08:16:12 +0200

    Drag and drop uploader

--- src/routes/PhotoUpload/PhotoUpload.tsx
+++ src/routes/PhotoUpload/PhotoUpload.tsx
@@ -1,5 +1,34 @@
+import { useDropzone } from "react-dropzone";
+import BackupIcon from "@mui/icons-material/Backup";
+
 const PhotoUpload = () => {
-  return <p>Here is photo upload</p>;
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map((file) => {
+    return (
+      <li key={file.path}>
+        <>
+          <img src={URL.createObjectURL(file)} alt="" width="300" />
+        </>
+      </li>
+    );
+  });
+
+  return (
+    <div className="drop-wrapper">
+      <section className="container">
+        <div {...getRootProps({ className: "dropzone" })}>
+          <input {...getInputProps()} />
+          <BackupIcon />
+          <p>Drag and drop some files here, or click to select files</p>
+        </div>
+      </section>
+      <div className="photos-list">
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoUpload;
